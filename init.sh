f1a129ea482ba5b634b6e582fc03b7ee6d3e1ec1
Erik Westlund <edbwestlund@gmail.com>
2024-03-23 19:00:34 -0400

    neovim fix

---
+++ init.sh
@@ -0,0 +1,6 @@
+sudo apt install -y python3-pip python3-venv ansible git neovim
+
+# Configure git
+git config --global user.email "edbwestlund@gmail.com"
+git config --global user.name "Erik Westlund"
+git config --global credential.helper store
